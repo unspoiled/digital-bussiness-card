36c1bef4ba1d35445f434dd9ed0bbbe651c308be
zac1182 <zac1182@gmail.com>
2021-11-29 17:11:41 +1300

    spelling

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,6 @@
 export default function About(){
     return(<div className="Main--About">
            <h3>About</h3>  
-           <p>I am a full stack developer ttrying to keep up with modern developments in front end technologies like react.js to stay relevant haha.</p>
+           <p>I am a full stack developer trying to keep up with modern developments in front end technologies like react.js to stay relevant haha.</p>
           </div>);
 }
